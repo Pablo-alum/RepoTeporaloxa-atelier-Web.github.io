35f9de596d76c110c3a0f4c189ad87acc9c149d4
Pablo ramos <benjapabloramos2002@gmail.com>
2023-06-07 17:15:59 -0300

    se modifcaron las productos

--- javascript/main.js
+++ javascript/main.js
@@ -3,21 +3,30 @@ createApp({
   data() {
     return {
       message: "Hello Vue!",
-     urlApi:"./javascript/prubas.json",
-    productos:[]
+      urlApi: "./javascript/prubas.json",
+      productos: [],
     };
   },
   created() {
-  this.Datos_Cargados(this.urlApi)
-
+    this.Datos_Cargados(this.urlApi);
   },
   mounted() {},
   methods: {
-    Datos_Cargados(url){
-      axios.get(url).then(response=>{
-        this.productos = response.data
-      })
-    }
-
+    //Datos cargados
+    Datos_Cargados(url) {
+      axios.get(url).then((response) => {
+        this.productos = response.data;
+      });
+    },
+    //iterador de itemes 
+    isThirdItem1(index1) {
+      return (index1 + 3) % 3 === 0;
+    },
+    isThirdItem2(index2) {
+      return (index2 + 2) % 3 === 0;
+    },
+    isThirdItem3(index3) {
+      return (index3 + 1) % 3 === 0;
+    },
   },
 }).mount("#app");
