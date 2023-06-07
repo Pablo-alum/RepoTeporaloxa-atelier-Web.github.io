12be7a037e91a5d1a72eeb95fb282fcc95570eba
Pablo ramos <benjapabloramos2002@gmail.com>
2023-06-07 01:27:13 -0300

    se gargan las distintas dimeciones con una sombra

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,23 @@
 const { createApp } = Vue;
-
 createApp({
   data() {
     return {
       message: "Hello Vue!",
+     urlApi:"./javascript/prubas.json",
+    productos:[]
     };
   },
-  created() {},
+  created() {
+  this.Datos_Cargados(this.urlApi)
+
+  },
   mounted() {},
-  methods: {},
+  methods: {
+    Datos_Cargados(url){
+      axios.get(url).then(response=>{
+        this.productos = response.data
+      })
+    }
+
+  },
 }).mount("#app");
